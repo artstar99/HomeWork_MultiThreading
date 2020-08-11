68ac834dea611c8ca4b7722a003f986a47b05742
artstar99 <artstar99@gmail.com>
2020-08-11 17:52:47 +0300

    Выполнено дополнительное задание домашней работы

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,15 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace Task_02
 {
     class Program
     {
-        static void Main(string[] args)
+        class CsvToTxt
         {
+            private StreamWriter writer;
+            private string inFile = @"..\..\..\..\Data\Data8277.csv";
+            private string outFile;
+
+            private int stringCounter;
+            private string writeString;
+            private bool writePermission;
+            private int strigBooferCapacity;
+
+            private TimeSpan elapsed;
+            private DateTime start;
+            public void Convert()
+            {
+                outFile = inFile.Replace(".csv", ".txt");
+                try
+                {
+                    using (var reader = new StreamReader(inFile))
+                    {
+                        writer = new StreamWriter(outFile, false);
+                        writePermission = true;
+                        strigBooferCapacity = 100;
+                        string bufferString = null;
+                        start = DateTime.Now;
+                        while (!reader.EndOfStream)
+                        {
+                            
+                            bufferString += $"{reader.ReadLine()}\n";
+                            
+                            stringCounter++;
+
+                            if (stringCounter == strigBooferCapacity || reader.EndOfStream)
+                            {
+                                elapsed = DateTime.Now - start;
+                                while (writePermission == false)
+                                {
+                                    Console.WriteLine("Запись не успевает за чтением");
+                                    strigBooferCapacity += 2;
+                                }
+
+                                strigBooferCapacity -= 1;
+                                Console.WriteLine($"Считано {stringCounter} строк. " +
+                                                  $"Потрачено времени {elapsed.Milliseconds}" +
+                                                  $" Произвожу запись");
+                                
+                                start = DateTime.Now;
+                                writeString = bufferString;
+                                bufferString = null;
+                                stringCounter = 0;
+
+                                Thread write = new Thread(Write);
+                                write.Start();
+                                writePermission = false;
+                            }
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw new Exception(e.Message);
+                }
+
+
+            }
+
+            private void Write()
+            {
+                try
+                {
+                    writer.WriteLine(writeString);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw new Exception(e.Message);
+                }
+
+                elapsed = DateTime.Now - start;
+                Console.WriteLine($"Запись произведена." +
+                                  $" Потрачено времени " +
+                                  $"{elapsed.Milliseconds}");
+                writePermission = true;
+                start=DateTime.Now;
+            }
+        }
+
+
+        static void Main()
+        {
+
+            CsvToTxt csvToTxt = new CsvToTxt();
+
+            csvToTxt.Convert();
+
+            Console.ReadLine();
         }
     }
 }
